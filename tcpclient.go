224d0c35f870ee83722cc7b1e41aeb857ee08079
李春辉 <lich@truhigh.com>
2020-06-08 14:47:09 +0800

    exchange the connection modem

--- tcpclient.go
+++ tcpclient.go
@@ -42,7 +42,7 @@ func NewTCPClientHandler(address string, rack int, slot int) *TCPClientHandler {
 	h.Address = address
 	h.Timeout = tcpTimeout
 	h.IdleTimeout = tcpIdleTimeout
-	h.ConnectionType = connectionTypePG // Connect to the PLC as a PG
+	h.ConnectionType = connectionTypeBasic // Connect to the PLC as a PG
 	remoteTSAP := uint16(h.ConnectionType)<<8 + (uint16(rack) * 0x20) + uint16(slot)
 	h.setConnectionParameters(address, 0x0100, remoteTSAP)
 	return h
